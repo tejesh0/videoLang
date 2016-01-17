7cbd5f1158e5a84913c096dfc0e2f4a8d83e43f4
tejesh95 <tejeshpapineni95@gmail.com>
2016-01-17 22:30:50 +0530

    basic video uploading done

--- video/models.py
+++ video/models.py
@@ -7,6 +7,10 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
+class Document(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+
+
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
